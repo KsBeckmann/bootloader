0771da54e084208ff9b7b1a651a5456d59844351
KsBeckmann <klaus.beckmann2005@gmail.com>
2026-06-08 00:38:37 -0300

    wip: breakout game with moving platform

--- boot.asm
+++ boot.asm
@@ -1,53 +1,93 @@
 bits 16
 org 0x7C00
 
-; setup
-xor ax, ax
-mov ds, ax
+; constants
+BLUE:  equ 0x09
+BROWN: equ 0x06
+
+; video mode
+xor ah, ah
+mov al, 0x13
+int 0x10
+
+; fill blue
+mov ax, 0xA000
 mov es, ax
-mov ss, ax
-mov sp, 0x7C00
+xor di, di
+mov cx, 0xFA00
+mov al, BLUE
+rep stosb
 
-; clear screen
-mov ah, 0x06
-mov al, 0x00
-mov bh, 0x0C
-xor cx, cx
-mov dh, 0x18
-mov dl, 0x4F
-int 0x10
+mov di, 0xE182
+mov al, BROWN
+call draw_rect
 
-; move cursor
-mov ah, 0x02
-xor bh, bh
-xor dx, dx
-mov dh, 0x0
-mov dl, 0x0
-int 0x10
-
-; print sting
-mov ah, 0x13
-mov al, 0x01
-xor bh, bh
-mov bl, 0x0C
-mov cx, 0x10
-xor dx, dx
-mov bp, string
-int 0x10
-
-; print keyboard input
-keyboard_loop:
-  xor ah, ah
+; keyboard input
+game_loop:
+  mov ah, 0x01
   int 0x16
-  mov ah, 0x0E
-  int 0x10
-  jmp keyboard_loop
+  jz game_loop
+  mov ah, 0x00
+  int 0x16
+  cmp ah, 0x4B
+  je left
+  cmp ah, 0x4D
+  je right
+  jmp game_loop
+
+left:
+  cmp word [plat_x], 0
+  jle game_loop
+
+  mov di, 0xE100
+  add di, [plat_x]
+  mov al, BLUE
+  call draw_rect
+
+  sub word [plat_x], 0x04
+
+  mov di, 0xE100
+  add di, [plat_x]
+  mov al, BROWN
+  call draw_rect
+  jmp game_loop
+
+right:
+  cmp word [plat_x], 0x104
+  jge game_loop
+
+  mov di, 0xE100
+  add di, [plat_x]
+  mov al, BLUE
+  call draw_rect
+
+  add word [plat_x], 0x04
+
+  mov di, 0xE100
+  add di, [plat_x]
+  mov al, BROWN
+  call draw_rect
+  jmp game_loop
 
 halt:
   jmp halt
 
-string:
-   db "type something: ", 0x00
+; DI -> initial position
+; AL -> color
+draw_rect:
+  mov cx, 0x08
+.line:
+  push cx
+  push di
+  mov cx, 0x3C
+  rep stosb
+  pop di
+  add di, 0x140
+  pop cx
+  loop .line
+  ret
+
+plat_x: dw 0x82
 
 times 510 - ($ - $$) db 0x00
 dw 0xAA55
