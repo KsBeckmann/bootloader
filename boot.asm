74896cc73d1d892c70f3444c316b2de30f68c2d8
KsBeckmann <klaus.beckmann2005@gmail.com>
2026-06-21 03:35:48 -0300

    testing cpuid

--- boot.asm
+++ boot.asm
@@ -1,93 +1,162 @@
 bits 16
 org 0x7C00
 
-; constants
-BLUE:  equ 0x09
-BROWN: equ 0x06
+jmp 0x0000:start
+start:
 
-; video mode
-xor ah, ah
-mov al, 0x13
+cli
+xor ax, ax
+mov ds, ax
+mov es, ax
+mov fs, ax
+mov gs, ax
+mov ss, ax
+mov sp, 0x7C00
+sti
+cld
+
+; clear screen
+mov ah, 0x06
+mov al, 0x00
+mov bh, 0x07
+xor cx, cx
+mov dh, 0x18
+mov dl, 0x4F
 int 0x10
 
-; fill blue
-mov ax, 0xA000
-mov es, ax
-xor di, di
-mov cx, 0xFA00
-mov al, BLUE
-rep stosb
+; move cursor to 0,0
+mov ah, 0x02
+xor bh, bh
+xor dx, dx
+int 0x10
 
-mov di, 0xE182
-mov al, BROWN
-call draw_rect
+; startup message
+mov si, starting_boot
+call print
 
-; keyboard input
-game_loop:
-  mov ah, 0x01
-  int 0x16
-  jz game_loop
-  mov ah, 0x00
-  int 0x16
-  cmp ah, 0x4B
-  je left
-  cmp ah, 0x4D
-  je right
-  jmp game_loop
+; check CPUID
+pushfd
+pop eax
+mov ecx, eax
+xor eax, 0x00200000
+push eax
+popfd
+pushfd
+pop eax
+xor eax, ecx
+and eax, 0x00200000
+jz no_cpuid
 
-left:
-  cmp word [plat_x], 0
-  jle game_loop
+mov si, cpuid_supported
+call print
 
-  mov di, 0xE100
-  add di, [plat_x]
-  mov al, BLUE
-  call draw_rect
-
-  sub word [plat_x], 0x04
-
-  mov di, 0xE100
-  add di, [plat_x]
-  mov al, BROWN
-  call draw_rect
-  jmp game_loop
-
-right:
-  cmp word [plat_x], 0x104
-  jge game_loop
-
-  mov di, 0xE100
-  add di, [plat_x]
-  mov al, BLUE
-  call draw_rect
-
-  add word [plat_x], 0x04
-
-  mov di, 0xE100
-  add di, [plat_x]
-  mov al, BROWN
-  call draw_rect
-  jmp game_loop
+call print_vendor
+call print_cpu_brand
 
 halt:
+  hlt
   jmp halt
 
-; DI -> initial position
-; AL -> color
-draw_rect:
-  mov cx, 0x08
-.line:
-  push cx
-  push di
-  mov cx, 0x3C
-  rep stosb
-  pop di
-  add di, 0x140
-  pop cx
-  loop .line
+no_cpuid:
+  mov si, cpuid_not_supported
+  call print
+  jmp halt
+
+print_vendor:
+  push eax
+  push ebx
+  push ecx
+  push edx
+
+  xor eax, eax
+  mov si, msg_vendor
+  call print
+  cpuid
+  mov [vendor_id+0], ebx
+  mov [vendor_id+4], edx
+  mov [vendor_id+8], ecx
+  mov si, vendor_id
+  call print
+
+  mov si, break_line
+  call print
+
+  pop edx
+  pop ecx
+  pop ebx
+  pop eax
   ret
 
-plat_x: dw 0x82
+print_cpu_brand:
+  push eax
+  push ebx
+  push ecx
+  push edx
+
+  mov si, msg_cpu_brand
+  call print
+  
+  mov eax, 0x80000002
+  cpuid
+  mov [cpu_brand+0],  eax
+  mov [cpu_brand+4],  ebx
+  mov [cpu_brand+8],  ecx
+  mov [cpu_brand+12], edx
+
+  mov eax, 0x80000003
+  cpuid
+  mov [cpu_brand+16],  eax
+  mov [cpu_brand+20],  ebx
+  mov [cpu_brand+24],  ecx
+  mov [cpu_brand+28], edx
+
+  mov eax, 0x80000004
+  cpuid
+  mov [cpu_brand+32],  eax
+  mov [cpu_brand+36],  ebx
+  mov [cpu_brand+40],  ecx
+  mov [cpu_brand+44],  edx
+
+  mov si, cpu_brand
+  call print
+
+  ; mov si, break_line
+  ; call print
+
+  pop edx
+  pop ecx
+  pop ebx
+  pop eax
+  ret
+
+; print a null-terminated string
+; input: SI -> string
+print:
+  push ax
+  push bx
+  push si
+  mov bh, 0
+.loop:
+  lodsb
+  cmp al, 0
+  je .done
+  mov ah, 0x0E
+  int 0x10
+  jmp .loop
+.done:
+  pop si
+  pop bx
+  pop ax
+  ret
+
+starting_boot:       db "starting bootloader...", 0x0D, 0x0A, 0
+cpuid_supported:     db "CPUID supported", 0x0D, 0x0A, 0
+cpuid_not_supported: db "CPUID NOT supported", 0x0D, 0x0A, 0
+msg_vendor:          db "vendor id: ", 0
+vendor_id:           times 13 db 0
+msg_cpu_brand:       db "cpu brand: ", 0
+cpu_brand:           times 49 db 0
+break_line:          db 0x0D, 0x0A, 0
 
 times 510 - ($ - $$) db 0x00
 dw 0xAA55
