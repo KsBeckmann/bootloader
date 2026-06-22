facdeadecf71c746149a2a5ad1c11e06a19777e6
KsBeckmann <klaus.beckmann2005@gmail.com>
2026-06-22 00:10:50 -0300

    load gdt table

--- boot.asm
+++ boot.asm
@@ -68,6 +68,11 @@ call enable_a20
 mov si, a20_enabled
 call print
 
+; GDT flat
+lgdt [gdt_descriptor]
+mov si, gdt_message
+call print
+
 halt:
   hlt
   jmp halt
@@ -111,6 +116,28 @@ cpuid_supported:     db "CPUID supported", 0x0D, 0x0A, 0
 cpuid_not_supported: db "CPUID NOT supported", 0x0D, 0x0A, 0
 a20_enabled:         db "A20 enabled", 0x0D, 0x0A, 0
 a20_disabled:        db "A20 disabled", 0x0D, 0x0A, 0
+gdt_message:         db "GDT loaded", 0x0D, 0x0A, 0
+
+gdt_start:
+  dq 0x0000000000000000
+  dw 0xFFFF
+  dw 0x0000
+  db 0x00
+  db 10011010b
+  db 11001111b
+  db 0x00
+
+  dw 0xFFFF
+  dw 0x0000
+  db 0x00
+  db 10010010b
+  db 11001111b
+  db 0x00
+gdt_end:
+
+gdt_descriptor:
+  dw gdt_end - gdt_start - 1
+  dd gdt_start
 
 times 510 - ($ - $$) db 0x00
 dw 0xAA55
